1a70a465657c92eec9cdd4bb232390f29a9815dd
xu.shuifeng <xu.shuifeng@immomo.com>
2019-05-06 17:56:57 +0800

    hide price remind entrance

--- StocksBar/ViewControllers/StocksTableViewController.swift
+++ StocksBar/ViewControllers/StocksTableViewController.swift
@@ -161,7 +161,7 @@ extension StocksTableViewController: NSMenuDelegate {
         menu.removeAllItems()
         menu.addItem(NSMenuItem(title: "置顶", action: #selector(handleTopRow), keyEquivalent: ""))
         menu.addItem(NSMenuItem(title: "删除", action: #selector(handleDeleteRow), keyEquivalent: ""))
-        menu.addItem(NSMenuItem(title: "股价提醒", action: #selector(priceReminder), keyEquivalent: ""))
+        //menu.addItem(NSMenuItem(title: "股价提醒", action: #selector(priceReminder), keyEquivalent: ""))
     }
     
     @objc private func handleDeleteRow() {
